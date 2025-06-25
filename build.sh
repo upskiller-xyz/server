79e7d713c5270437f2da917b7733dbbfe64ba271
STASYA00 <0.0stasya@gmail.com>
2025-06-25 15:38:12 +0200

    refactor: adjust cloudbuild files to the new file structure

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-export DAYLIGHT_SERVER_VERSION=$(cat __version__.py | cut -d '=' -f2 | xargs)
+export DAYLIGHT_SERVER_VERSION=$(cat src/__version__.py | cut -d '=' -f2 | xargs)
 source .env
 gcloud builds submit --region=${GCP_REGION} --substitutions=_DAYLIGHT_SERVER_VERSION=${DAYLIGHT_SERVER_VERSION},_SERVER_NAME=${SERVER_NAME},_IMAGE_NAME=${IMAGE_NAME},_GCP_REGION=${GCP_REGION},_REPO_NAME=${REPO_NAME} --config imagebuild.yaml
 # sed -r 's/DAYLIGHT_SERVER_VERSION/'"$DAYLIGHT_SERVER_VERSION"'/' imagebuild.yaml > container_versioned.yaml
